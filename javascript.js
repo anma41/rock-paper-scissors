25b354387fa51cfbdc1db5e8dc5b8a66a0eedd82
anma41 <118857442+anma41@users.noreply.github.com>
2023-01-20 17:04:29 +0100

    function playRound added

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let x=129;
-console.log(x);
-console.log("osem");
 let CPUitems=["rock","paper","scissors"];
 let CPUpick;
 function getComputerChoice() {
@@ -8,54 +5,74 @@ function getComputerChoice() {
     return CPUpick;
 }
 getComputerChoice();
-console.log(CPUpick);
+//console.log(CPUpick);
 
 
 let playerSelection=prompt("Prosim izberi svoje orožje");
+function playRound(playerSelection,CPUpick) {
 if (playerSelection.toLowerCase() === "rock") {
     if (CPUpick==="rock") {
-        console.log("izenačenje");
+        return "Tie";
         }
     else if (CPUpick==="paper") {
+        return "Lost";
         console.log("Izgubil si!");
     }
     else if (CPUpick==="scissors") {
+        return "Win";
         console.log("Bravo, zmaga!");
     }
     else {
-        console.log("Ne igraš po pravilih")
+        console.log("Neregistrirano orožje.")
     }  
 }
 else if (playerSelection.toLowerCase() === "paper") {
     if (CPUpick==="paper") {
+        return "Tie";
         console.log("izenačenje");
         }
     else if (CPUpick==="scissors") {
+        return "Lost";
+
         console.log("Izgubil si!");
     }
     else if (CPUpick==="rock") {
+        return "Win";
         console.log("Bravo, zmaga!");
     }
     else {
-        console.log("Ne igraš po pravilih")
+        return "Wrong item!";
+        console.log("Neregistrirano orožje.");
     } 
 }
 
 else if (playerSelection.toLowerCase() === "scissors") {
     if (CPUpick==="scissors") {
+        return "Tie";
         console.log("izenačenje");
         }
     else if (CPUpick==="rock") {
+        return "Lost";
         console.log("Izgubil si!");
-    }
+            }
     else if (CPUpick==="paper") {
+        return "Win";
         console.log("Bravo, zmaga!");
     } 
 }
 else
     {console.log("Neregistrirano orožje.")
     }
-    console.log(playerSelection);
+}
+
+console.log("Izbor računalnika: " +CPUpick);
+console.log("Tvoja izbira: " +playerSelection);
+playRound(playerSelection,CPUpick);
+console.log(playRound(playerSelection, CPUpick));
+
+
+
+
 /*if (playerSelection !== "scissors" || "rock"  || "paper") {
     console.log(playerSelection);
     console.log("uporabljaš neregistrirano orožje");
