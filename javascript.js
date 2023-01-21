9305434e9a24556dc4ad1dc0a67b9c6675ad4c4b
anma41 <118857442+anma41@users.noreply.github.com>
2023-01-21 11:14:16 +0100

    Code cleaned

--- javascript.js
+++ javascript.js
@@ -1,19 +1,17 @@
+/*computer picks one of 3 items randomly*/
 let CPUitems=["rock","paper","scissors"];
 let CPUpick;
 function getComputerChoice() {
     CPUpick=CPUitems[Math.floor(Math.random()*CPUitems.length)];
     return CPUpick;
 }
-//getComputerChoice();
-//console.log(CPUpick);
 
-playerSelection="orožje";
-//let playerSelection="prazno";//=prompt("Prosim izberi svoje orožje");
-//function playerSel(playerSelection) {
-  //  playerSelection=prompt("Prosim izberi svoje orožje");
-//}
-//playerSel(playerSelection);
-//console.log(playerSel);
+/*playerSelection later gets its variable from prompt*/
+playerSelection="weapon";
+
+/* playerRound = 1 => zmaga
+   playerRound = 2 => poraz
+   playerRound = 0 => draw */ 
 function playRound(playerSelection,CPUpick) {
     if (playerSelection.toLowerCase() === "rock") {
             if (CPUpick==="rock") {
@@ -24,50 +22,42 @@ function playRound(playerSelection,CPUpick) {
             }
             else if (CPUpick==="scissors") {
                 return 1;
-                console.log("Bravo, zmaga!");
-            }
+                }
     }
     else if (playerSelection.toLowerCase() === "paper") {
         if (CPUpick==="paper") {
             return 0;
-            console.log("izenačenje");
+   //         console.log("izenačenje");
             }
         else if (CPUpick==="scissors") {
             return 2;
-
-            console.log("Izgubil si!");
+//            console.log("Izgubil si!");
         }
         else if (CPUpick==="rock") {
             return 1;
-            console.log("Bravo, zmaga!");
+ //           console.log("Bravo, zmaga!");
         }
     }
 
 else if (playerSelection.toLowerCase() === "scissors") {
     if (CPUpick==="scissors") {
-        console.log("izenačenje");
+     //   console.log("izenačenje");
         return 0;
                 }
     else if (CPUpick==="rock") {
-        console.log("Izgubil si!");
+      //  console.log("Izgubil si!");
         return 2;
             }
     else if (CPUpick==="paper") {
-        console.log("Bravo, zmaga!");
+      //  console.log("Bravo, zmaga!");
         return 1;
     } 
 }
 else
-    {//console.log("Neregistrirano orožje.")
+    {
     return 4;
     }
 }
-
-
-//console.log("Izbor računalnika: " +CPUpick);
-//console.log("Tvoja izbira: " +playerSelection);
-//playRound(playerSelection,CPUpick);
-//console.log(playRound(playerSelection, CPUpick));
 
 let stevecWin=0;
 let stevecLost=0;
@@ -103,7 +93,8 @@ function game() {
     
     
     console.log("RESULT ::: Player: "+stevecWin+" ::: Computer: "+stevecLost+" ::: Draw: "+stevecDraw);
-    console.log(whowin);
+    console.log(' ');//new line in console for cleaner view.
+    //console.log(whowin);
     if(stevecWin>=dokoliko) {
         console.log("WIN!!!");
     }
@@ -116,51 +107,3 @@ function game() {
 }
 }
 game();
-  /*  )
-    if rezultat = 1 {
-        for (i=0; i < 5; i++) {
-    playRound(playerSelection,CPUpick)
-    } 
-}
-}
-console.log(game());
-console.log(i);
-/*<script>
-     let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-     for (let i=1; i<= answer; i++) {
-        if (i%15===0) {
-            console.log(i, "FizzBuzz");
-        } 
-        else if (i % 3===0) {
-            console.log(i, "Fizz");
-        }
-        else if (i % 5===0) {    
-        console.log(i, " Buzz");
-     }
-    }
-    //comment najprej določi spremenljivko in ji definiraj začetno vrednost
-    let x=0;
-    let y=0;
-    let sum=x+y;
-    console.log(sum);
-
-    
-    
-    </script>*/
-
-/*if (playerSelection !== "scissors" || "rock"  || "paper") {
-    console.log(playerSelection);
-    console.log("uporabljaš neregistrirano orožje");
-} */
-/*function playerSelection () {
-
-}*/
-/*playerSelection
-computerSelection
-return string ("You lose! Paper beats Rock!") OR "YOU WIN!";*/
-//case nonsensitive selection
-/*
-za štetje koliko iger smo šli, naredimo z loopi (for..) igramo do 5.
-function game()
-prompt() za userjev input
-*/
